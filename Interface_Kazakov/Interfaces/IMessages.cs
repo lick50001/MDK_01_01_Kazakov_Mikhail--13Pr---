2ae4e74207bddd02147932d4734ab0e95a8c0605
vannn1sh <dubovan36@gmail.com>
2025-10-27 17:54:43 +0500

    Реализованные интерфейсы. Казаков

---
+++ Interface_Kazakov/Interfaces/IMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface_Kazakov.Models;
+
+namespace Interface_Kazakov.Interfaces
+{
+    public class IMessages
+    {
+        void All(out List<Messages> Messages);
+
+        void Save(bool Update = false);
+
+        void Delete();
+    }
+}
