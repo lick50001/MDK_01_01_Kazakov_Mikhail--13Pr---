60e3d48724e89781fa2639edb44ac193e3f81dda
vannn1sh <dubovan36@gmail.com>
2025-10-27 17:52:14 +0500

    Реализованные модели. Казаков

---
+++ Interface_Kazakov/Models/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Kazakov.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Create {  get; set; }
+
+        public int IdUser { get; set; }
+        public Messages() { }
+
+        public Messages(int Id, DateTime Create, int IdUser)
+        {
+            this.Id = Id;
+            this.Create = Create;
+            this.IdUser = IdUser;
+        }
+    }
+}
---
+++ Interface_Kazakov/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Kazakov.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Users() { }
+        public Users(int Id, string FIO)
+        {
+            this.Id = Id;
+            this.FIO = FIO;
+        }
+    
+    }
+}
